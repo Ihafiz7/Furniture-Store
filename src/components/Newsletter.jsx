e37d4dcd226ecb47b9b297cfeed65740245cbe08
Hafiz Al Asad <hafizalasad712@gmail.com>
2024-05-12 19:17:17 +0600

    Update in Newsletter.jsx

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -5,7 +5,7 @@ const Newsletter = () => {
     <div className='flex flex-col items-center justify-center gap-8 font-overpass sm:my-4 xl:my-16'>
         <h1 className='text-4xl font-semibold tracking-wide'>Join our mailing list</h1>
         <h3 className='text-2xl font-normal text-gray-400 sm:w-3/4 xl:w-1/2 text-center'>Sign up to receive inspiration, product updates, and special offers from our team.</h3>
-        <div className='flex items-center sm:flex-col xl:flex-row gap-4 w-full sm:px-2 xl:px-0'>
+        <div className='flex items-center justify-center sm:flex-col xl:flex-row gap-4 w-full sm:px-2 xl:px-0'>
             <input type="email" placeholder='Email Address' className='sm:w-full xl:w-[350px] p-4 focus:outline-none border border-gray-400 text-xl rounded-sm' />
             <button className='sm:w-full xl:w-[200px] h-16 p-2 bg-black text-white rounded-md text-xl font-medium hover:opacity-80 hover:transition-all hover:text-green-600'>SUBSCRIBE</button>
         </div>
